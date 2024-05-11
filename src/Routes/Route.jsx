c9b48c22636ab6b08ddf15372047bf0c046a0057
rjangid9080 <grut.rjangid9080@gmail.com>
2024-05-11 12:47:50 +0530

    Initial Commit.

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import React from "react";
+
+export default function Route() {
+  return (
+    <>
+      <BrowserRouter></BrowserRouter>
+    </>
+  );
+}
